300aad5d1711d8682a8353e6772206f2b8e6dd9a
hrithhik-10 <hrithikppawarpatil7@gmail.com>
2024-02-23 01:00:58 +0530

    """"

--- tf_script.tf
+++ tf_script.tf
@@ -7,9 +7,9 @@ resource "aws_instance" "my_instance" {
     ami = "ami-0156b61643fdfee5c"
     instance_type = "t2.micro"
     key_name = "canada.ky"
-    tags = {
-      env = "dev"
-      name = "instance_1"
-      vpc_security_group_ids = ["sg-0f75e427e52c76d89"] 
-    }
+     tags = {
+         env = "dev"
+         Name = "instance_1"
 }  
+    vpc_security_group_ids = vpc_ = ["sg-0f75e427e52c76d89"] 
+}
