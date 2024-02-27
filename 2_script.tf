a04081c7a2d25138e6649f87b9e5935d3f3316e6
hrithhik-10 <hrithikppawarpatil7@gmail.com>
2024-02-27 15:49:52 +0530

    new script

---
+++ 2_script.tf
@@ -0,0 +1,21 @@
+
+provider  "aws" {
+    region = "ca-central-1"
+}
+
+resource "aws_instance" "my_instance" {
+    ami = "ami-0156b61643fdfee5c"
+    instance_type = "t2.micro"
+    key_name = "canada.ky"
+     tags = {
+         env = "dev"
+         Name = "instance_2"
+}  
+    vpc_security_group_ids = ["sg-0f75e427e52c76d89"]
+
+    
+}
+variable "region" {
+  description = "please enter aws region"
+  default = ""
+}
