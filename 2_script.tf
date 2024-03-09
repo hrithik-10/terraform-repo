2db1207258c7b1bfc7956bf1830e248cb49d3895
hrithhik-10 <hrithikppawarpatil7@gmail.com>
2024-03-09 14:34:40 +0530

    error resovled

--- 2_script.tf
+++ 2_script.tf
@@ -1,21 +1,37 @@
+terraform {
+  backend "s3" {
+    bucket = "terraform-backup-mumbai"
+    region = "Asia Pacific (Mumbai) ap-south-1"
+    key = "value"
 
-provider  "aws" {
-    region = "ca-central-1"
+  }
 }
 
-resource "aws_instance" "my_instance" {
-    ami = "ami-0156b61643fdfee5c"
-    instance_type = "t2.micro"
-    key_name = "canada.ky"
-     tags = {
-         env = "dev"
-         Name = "instance_2"
-}  
-    vpc_security_group_ids = ["sg-0f75e427e52c76d89"]
+provider aws {
+    region = var.region
+}
 
-    
+resource "aws instance" "my_instance" {
+    ami = "ami-0e670eb768a5fc3d4"
+    instance_type = var.instance_type
+    key_name = "var.key"
+    tags = {
+        env = "dev"
+        Name = "instance-2"
+    }
+    vpc_security_group_ids = ["sg-0465b2648fc3cac08"]
+
 }
-variable "region" {
-  description = "please enter aws region"
-  default = ""
-}
+ variable "region" {
+    description = "please entervaws region"
+    default = "ap-south-1"
+ }
+
+ variable "instance_type" {
+    description = "instance_type"
+    default = "t2.micro"
+ }
+
+ variable "var.key" {
+    default = "teraform.ky"
+ }
